78f5ad11d7b198b063b2ad63297b71b9fed62ad1
Dolon Roy <dolonr718@gmail.com>
2024-01-20 21:18:49 +0600

    task complete marking functinality added commit

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ const handleDeleteTask = (id) => {
     task.remove();
 }
 
+const handleCompleteTask = (id) => {
+    const taskParent = document.querySelector(`.task-${id}`)
+    const task = document.querySelector(`#task-checkbox-${id}`);
+    
+    if(task.checked) {
+        const taskText = taskParent.querySelector(".task-text");
+        taskText.style.textDecoration = "line-through";
+    }
+}
+
 
 // validation and task adding functionality is here
 submitBtn.addEventListener("click", () => {
@@ -23,7 +33,7 @@ submitBtn.addEventListener("click", () => {
     taskItem.classList.add("task", `task-${id}`);
 
     taskItem.innerHTML = `
-        <input type="checkbox" name="" id="">
+        <input type="checkbox" name="" onclick="handleCompleteTask(${id})" id="task-checkbox-${id}">
         <h3 class="task-text">${taskText}</h3>
         <button onclick="handleDeleteTask(${id})" class="option-btn">
             <img width="20px" src="images/trash.png" alt="">
