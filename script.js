716d30787674968c038cb99516388a91b3219c28
Dolon Roy <dolonr718@gmail.com>
2024-01-20 21:02:15 +0600

    task delete functionality added commit

--- script.js
+++ script.js
@@ -2,25 +2,37 @@
 const taskInput = document.querySelector("#task-input");
 const submitBtn = document.querySelector(".input_submit");
 const taskContainer = document.querySelector("#task-container");
+let id = 1;
+
+
+// task delete functionality is here
+const handleDeleteTask = (id) => {
+    const task = document.querySelector(`.task-${id}`);
+    task.remove();
+}
+
 
 // validation and task adding functionality is here
 submitBtn.addEventListener("click", () => {
+
   const taskText = taskInput?.value;
 
   if (taskText?.length > 0) {
 
     const taskItem = document.createElement("div");
-    taskItem.classList.add("task");
+    taskItem.classList.add("task", `task-${id}`);
+
     taskItem.innerHTML = `
         <input type="checkbox" name="" id="">
-        <h3 class="task-text">Food ordering</h3>
-        <button class="option-btn">
+        <h3 class="task-text">${taskText}</h3>
+        <button onclick="handleDeleteTask(${id})" class="option-btn">
             <img width="20px" src="images/trash.png" alt="">
         </button>
     `;
 
     taskContainer.appendChild(taskItem);
-    taskInput.value = ""
+    taskInput.value = "";
+    id++;
   } else {
     alert("Please input your task!");
   }
